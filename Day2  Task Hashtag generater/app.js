c9755577d5e350e4eea43a25cbb50f7f24128f24
akhterjawad <akhterjawad566@gmail.com>
2024-07-17 03:19:43 +0500

    omplete

--- Day2  Task Hashtag generater/app.js
+++ Day2  Task Hashtag generater/app.js
@@ -9,19 +9,20 @@
 //? Otherwise, the function should return the generated hash tag prefixed with #.
 
 //* Write a function generateHash to accomplish this task.
-let h2 = document.querySelector('h2')
+let h2 = document.querySelector('h2');
 let ArrayWords;
 function generateHash(string) {
     if (string.length > 280 || string.trim().length === 0) {
         return false
     }
-    ArrayWords = string.split(" ")
+    ArrayWords = string.split(" ");
 
     ArrayWords = ArrayWords.map(CurrentW => CurrentW.replace(CurrentW[0], CurrentW[0].toUpperCase()));
 
-    return ArrayWords = `#${ArrayWords.join('')}`
+    return ArrayWords = `#${ArrayWords.join('')}`;
 }
-let main = prompt('.')
+let main = prompt('Enter as you want to make a hashtag');
 generateHash(`${main}`);
 // o/p = "#MyNameIs Thapa Technical"
-h2.innerHTML = ArrayWords
+h2.innerHTML = ArrayWords;
+console.log(ArrayWords);
